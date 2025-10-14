b416e05397cc146be2c73e2cb70ab1fc78c5e2c0
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-14 15:46:59 +0300

    Добавлені редіректи після виходу, входу в аккаунт і login_url

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "users:profile"
+LOGOUT_REDIRECT_URL = "users:login"
+LOGIN_URL = "users:login"
