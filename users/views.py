c4428c62858785efe6aed8cb18a6035c24426f1f
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-16 21:26:18 +0300

    added functions (registration, profile)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
-# Create your views here.
+def registration(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("users:profile")
+        else:
+            form= SignUpForm()
+    return render(request, "user/registration.html", {"form" : form})
+
+@login_required
+def profile(request):
+    return render(request, "user/profile.html")
