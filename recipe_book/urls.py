954deae80ae865b43eeea65dd75a5357c11e33bb
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-17 15:55:12 +0300

    recipes/ url

--- recipe_book/urls.py
+++ recipe_book/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include(("users.urls", "users"), namespace="users")),
-    # path("recipes/", include(("recipes.urls", "recipes"), namespace="recipes"))
+    path("recipes/", include(("recipes.urls", "recipes"), namespace="recipes"))
 ]
