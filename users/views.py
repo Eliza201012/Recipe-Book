1f9c89ffac42945f69d3988179ec532132314276
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-17 10:58:32 +0300

    added function custom_logout and message for registration function

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import SignUpForm
 
 def home(request):
@@ -12,6 +13,7 @@ def registration(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "You successfully login.")
             return redirect("users:profile")
     else:
         form= SignUpForm()
@@ -20,3 +22,10 @@ def registration(request):
 @login_required
 def profile(request):
     return render(request, "users/profile.html")
+
+def custom_logout(request):
+    if request.method == "POST":
+        logout(request)
+        messages.success(request, "You successfully logout.")
+        return redirect("users:home")
+    return redirect("users:login")
