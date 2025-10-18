51fcf79eaf663fb18b48e9894afda7b948791b02
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-18 12:32:04 +0300

    created models Recipe and Book

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ("breakfast", "Breakfast"),
+        ("lunch", "Lunch"),
+        ("dinner", "Dinner"),
+        ("dessert", "Dessert"),
+        ("drinks", "Drinks"),
+        ("high-protein food", "High-protein food")
+    ]
+    title = models.CharField(verbose_name="Title", max_length=200)
+    cooking_time = models.TimeField(verbose_name="Cooking time")
+    description = models.TextField(verbose_name="Description")
+    instructions = models.TextField(verbose_name="Steps one by one")
+    category = models.CharField(verbose_name="Category", choices=CATEGORY_CHOICES, default="breakfast")
+    created_at = models.DateField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateField(verbose_name="Updated at", auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Recipe"
+        verbose_name_plural = "Recipes"
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=150)
+    recipe = models.ForeignKey(Recipe, verbose_name="Recipe", on_delete=models.CASCADE, related_name="books")
+    created_at = models.DateField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateField(verbose_name="Updated at", auto_now=True)
