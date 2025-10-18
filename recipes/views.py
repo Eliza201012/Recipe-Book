e8d4b947fb0d646ce3ae8527f32201c45f787636
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-18 15:03:49 +0300

    реалізован CRUD для рецептів (create_recipe, recipe_list, recipe_detail, recipe_update, recipe_delete)

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe, Book
+from .forms import RecipeForm, BookForm
 
-# Create your views here.
+# CREATE (Recipe)
+def create_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("recipes:recipe_list")
+    else:
+        form = RecipeForm()
+    return render(request, "recipes/recipe_form.html", {"form" : form})
+
+# READ (Recipe list)
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, "recipes/recipe_list.html", {"recipes" : recipes})
+
+# READ (Recipe detail)
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, "recipes/recipe_detail.html", {"recipe" : recipe})
+
+# UPDATE
+def recipe_update(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=Recipe)
+        if form.is_valid():
+            form.save()
+            return redirect("recipes:recipe_list")
+    else:
+        form = RecipeForm(instance=Recipe)
+    return render(request, "recipes/recipe_form.html", {"form" : form})
+
+# DELETE
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == "POST":
+        recipe.delete()
+        return redirect("recipes:recipe_list")
+    return render(request, "recipes/recipe_confirm_delete.html", {"recipe" : recipe})
