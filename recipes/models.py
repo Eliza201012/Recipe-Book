2b332672a20c434064abf03c42dd400a021ceadb
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-10-21 22:03:59 +0300

    changed fields title and book

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=150)
+    created_at = models.DateField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateField(verbose_name="Updated at", auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+
 class Recipe(models.Model):
     CATEGORY_CHOICES = [
         ("breakfast", "Breakfast"),
@@ -10,6 +24,7 @@ class Recipe(models.Model):
         ("high-protein food", "High-protein food")
     ]
     title = models.CharField(verbose_name="Title", max_length=200)
+    book = models.ForeignKey(Book, verbose_name="Book", on_delete=models.CASCADE, related_name="recipes")
     cooking_time = models.TimeField(verbose_name="Cooking time")
     description = models.TextField(verbose_name="Description")
     instructions = models.TextField(verbose_name="Steps one by one")
@@ -24,18 +39,3 @@ class Recipe(models.Model):
         ordering = ["-created_at"]
         verbose_name = "Recipe"
         verbose_name_plural = "Recipes"
-
-
-class Book(models.Model):
-    title = models.CharField(verbose_name="Title", max_length=150)
-    recipe = models.ForeignKey(Recipe, verbose_name="Recipe", on_delete=models.CASCADE, related_name="books")
-    created_at = models.DateField(verbose_name="Created at", auto_now_add=True)
-    updated_at = models.DateField(verbose_name="Updated at", auto_now=True)
-
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        ordering = ["-created_at"]
-        verbose_name = "Book"
-        verbose_name_plural = "Books"
